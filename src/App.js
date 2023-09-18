5665edef3b5382316d5916702c03c70457e64ccf
keerthanaj2004 <jkeerthana2004@gmail.com>
2023-09-18 21:50:26 +0530

    prop commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 //babel compiles the js code
 function App() {
   return (
     //shld return only one element here returning div
     //node module waala folder kisiko dete nhi
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TExT Ex</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-    </div>
-    </nav>
+    <Navbar title="TextEx" search="Explore"/>
     </>
   );
 }
